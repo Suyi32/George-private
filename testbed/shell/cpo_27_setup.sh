faa11944d389c8a72591412b84b68810d8cd5c89
茶木 <chamu.wlp@alibaba-inc.com>
2020-10-14 20:11:31 +0800

    batch size mtv examples

--- testbed/shell/cpo_27_setup.sh
+++ testbed/shell/cpo_27_setup.sh
@@ -14,11 +14,12 @@ cd /workspace/George-private/testbed
 screen -dmS MySessionName0 &
 screen -dmS MySessionName1 &
 screen -dmS MySessionName2 &
-screen -S MySessionName0 -p 0 -X stuff "cd /workspace/George-private/testbed;   sh bin/cpo_27_more.sh 0
+screen -S MySessionName0 -p 0 -X stuff "cd /workspace/George-private/testbed;
+   sh shell/cpo_27_more.sh 0
 " &
-screen -S MySessionName1 -p 0 -X stuff "cd /workspace/George-private/testbed;  sh bin/cpo_27_more.sh 10
+screen -S MySessionName1 -p 0 -X stuff "cd /workspace/George-private/testbed;  sh shell/cpo_27_more.sh 10
 " &
-screen -S MySessionName2 -p 0 -X stuff "cd /workspace/George-private/testbed;  sh bin/cpo_27_more.sh 20
+screen -S MySessionName2 -p 0 -X stuff "cd /workspace/George-private/testbed;  sh shell/cpo_27_more.sh 20
 "
 screen -r MySessionName0
 
