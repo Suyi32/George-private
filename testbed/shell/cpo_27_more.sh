51a8edf495307f2357394f88375bef2560effa3a
茶木 <chamu.wlp@alibaba-inc.com>
2020-10-14 20:05:07 +0800

    batch size mtv examples

--- testbed/shell/cpo_27_more.sh
+++ testbed/shell/cpo_27_more.sh
@@ -4,7 +4,8 @@ pathname="CPO_27_more.py"
 
 for VARIABLE in 0 1 2 3 4 5 6 7 8 9
 do
-python3 $pathname --batch_choice $(($VARIABLE + $1))
+#python3 $pathname --batch_choice $(($VARIABLE + $1))
+python3 -u -W ignore PPPOWithoutSubScheduler_PCPO_PPO.py --batch_choice $(($VARIABLE + $1)) --clip_eps 0.1 --safety_requirement 0.02 --lr 0.001
 done
 
 # etc.
--- testbed/shell/cpo_27_setup.sh
+++ testbed/shell/cpo_27_setup.sh
@@ -5,8 +5,8 @@ ps -ef | grep python
 pkill screen
 pkill python
 pkill pyhon3
-cd /home/ubuntu/atc/testbed
-git pull
+cd /workspace/George-private/testbed
+#git pull
 
 
 
@@ -14,11 +14,11 @@ git pull
 screen -dmS MySessionName0 &
 screen -dmS MySessionName1 &
 screen -dmS MySessionName2 &
-screen -S MySessionName0 -p 0 -X stuff "cd /home/ubuntu/atc/testbed;  sh cpo_27_more.sh 0
+screen -S MySessionName0 -p 0 -X stuff "cd /workspace/George-private/testbed;  sh cpo_27_more.sh 0
 " &
-screen -S MySessionName1 -p 0 -X stuff "cd /home/ubuntu/atc/testbed;  sh cpo_27_more.sh 10
+screen -S MySessionName1 -p 0 -X stuff "cd /workspace/George-private/testbed;  sh cpo_27_more.sh 10
 " &
-screen -S MySessionName2 -p 0 -X stuff "cd /home/ubuntu/atc/testbed;  sh cpo_27_more.sh 20
+screen -S MySessionName2 -p 0 -X stuff "cd /workspace/George-private/testbed;  sh cpo_27_more.sh 20
 "
 screen -r MySessionName0
 
