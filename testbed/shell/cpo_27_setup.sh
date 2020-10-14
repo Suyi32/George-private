71d73b819ee0e49272d3cf7bb9168d9dceb16690
茶木 <chamu.wlp@alibaba-inc.com>
2020-10-14 20:34:18 +0800

    batch size mtv examples

--- testbed/shell/cpo_27_setup.sh
+++ testbed/shell/cpo_27_setup.sh
@@ -14,12 +14,26 @@ cd /workspace/George-private/testbed
 screen -dmS MySessionName0 &
 screen -dmS MySessionName1 &
 screen -dmS MySessionName2 &
+screen -dmS MySessionName4 &
+screen -dmS MySessionName5 &
+screen -dmS MySessionName3 &
 screen -S MySessionName0 -p 0 -X stuff "cd /workspace/George-private/testbed;
-   sh shell/cpo_27_more.sh 0
+sh shell/cpo_27_more.sh 0
 " &
-screen -S MySessionName1 -p 0 -X stuff "cd /workspace/George-private/testbed;  sh shell/cpo_27_more.sh 10
+screen -S MySessionName1 -p 0 -X stuff "cd /workspace/George-private/testbed;
+  sh shell/cpo_27_more.sh 5
 " &
-screen -S MySessionName2 -p 0 -X stuff "cd /workspace/George-private/testbed;  sh shell/cpo_27_more.sh 20
+screen -S MySessionName2 -p 0 -X stuff "cd /workspace/George-private/testbed;
+  sh shell/cpo_27_more.sh 10
+" &
+screen -S MySessionName3 -p 0 -X stuff "cd /workspace/George-private/testbed;
+sh shell/cpo_27_more.sh 15
+" &
+screen -S MySessionName4 -p 0 -X stuff "cd /workspace/George-private/testbed;
+  sh shell/cpo_27_more.sh 20
+" &
+screen -S MySessionName5 -p 0 -X stuff "cd /workspace/George-private/testbed;
+  sh shell/cpo_27_more.sh 25
 "
-screen -r MySessionName0
+#screen -r MySessionName0
 
