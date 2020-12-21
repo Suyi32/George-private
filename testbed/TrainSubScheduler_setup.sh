cf36cc00ece5f5a905d511269399202eb16b3fff
Luping <luping@B-FFY0ML7H-0057.local>
2020-12-21 19:45:48 +0800

    729

--- testbed/TrainSubScheduler_setup.sh
+++ testbed/TrainSubScheduler_setup.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-pathname="./UnifiedModel_PCPO_PPO_ray_ilp.py"
+pathname="./UnifiedModel_PCPO_PPO_ilp.py"
 
 screen -dmS MySessionName0 &
 screen -dmS MySessionName1 &
@@ -18,28 +18,28 @@ screen -dmS MySessionName10 &
 #screen -dmS MySessionName12 &
 #screen -dmS MySessionName13 &
 #screen -dmS MySessionName14 &
-screen -S MySessionName0 -p 0 -X stuff " cd /workspace/George-private/testbed;  python3 ./UnifiedModel_PCPO_PPO_ray_ilp.py --start_sample 0
+screen -S MySessionName0 -p 0 -X stuff " cd /workspace/George-private/testbed;  python3 ./UnifiedModel_PCPO_PPO_ilp.py --start_sample 0
 " &
-screen -S MySessionName1 -p 0 -X stuff " cd /workspace/George-private/testbed;  python3 ./UnifiedModel_PCPO_PPO_ray_ilp.py --start_sample 10
+screen -S MySessionName1 -p 0 -X stuff " cd /workspace/George-private/testbed;  python3 ./UnifiedModel_PCPO_PPO_ilp.py --start_sample 10
 "&
-screen -S MySessionName2 -p 0 -X stuff " cd /workspace/George-private/testbed;  python3 ./UnifiedModel_PCPO_PPO_ray_ilp.py --start_sample 20
+screen -S MySessionName2 -p 0 -X stuff " cd /workspace/George-private/testbed;  python3 ./UnifiedModel_PCPO_PPO_ilp.py --start_sample 20
 "&
-screen -S MySessionName3 -p 0 -X stuff " cd /workspace/George-private/testbed;  python3 ./UnifiedModel_PCPO_PPO_ray_ilp.py --start_sample 30
+screen -S MySessionName3 -p 0 -X stuff " cd /workspace/George-private/testbed;  python3 ./UnifiedModel_PCPO_PPO_ilp.py --start_sample 30
 " &
 
-screen -S MySessionName4 -p 0 -X stuff " cd /workspace/George-private/testbed;  python3 ./UnifiedModel_PCPO_PPO_ray_ilp.py --start_sample 40
+screen -S MySessionName4 -p 0 -X stuff " cd /workspace/George-private/testbed;  python3 ./UnifiedModel_PCPO_PPO_ilp.py --start_sample 40
 "&
-screen -S MySessionName5 -p 0 -X stuff " cd /workspace/George-private/testbed;  python3 ./UnifiedModel_PCPO_PPO_ray_ilp.py --start_sample 50
+screen -S MySessionName5 -p 0 -X stuff " cd /workspace/George-private/testbed;  python3 ./UnifiedModel_PCPO_PPO_ilp.py --start_sample 50
 "&
-screen -S MySessionName6 -p 0 -X stuff " cd /workspace/George-private/testbed;  python3 ./UnifiedModel_PCPO_PPO_ray_ilp.py --start_sample 60
+screen -S MySessionName6 -p 0 -X stuff " cd /workspace/George-private/testbed;  python3 ./UnifiedModel_PCPO_PPO_ilp.py --start_sample 60
 "&
-screen -S MySessionName7 -p 0 -X stuff " cd /workspace/George-private/testbed;  python3 ./UnifiedModel_PCPO_PPO_ray_ilp.py --start_sample 70
+screen -S MySessionName7 -p 0 -X stuff " cd /workspace/George-private/testbed;  python3 ./UnifiedModel_PCPO_PPO_ilp.py --start_sample 70
 "&
 
 
-screen -S MySessionName8 -p 0 -X stuff " cd /workspace/George-private/testbed;  python3 ./UnifiedModel_PCPO_PPO_ray_ilp.py --start_sample 80
+screen -S MySessionName8 -p 0 -X stuff " cd /workspace/George-private/testbed;  python3 ./UnifiedModel_PCPO_PPO_ilp.py --start_sample 80
 "&
-screen -S MySessionName9 -p 0 -X stuff " cd /workspace/George-private/testbed;  python3 ./UnifiedModel_PCPO_PPO_ray_ilp.py --start_sample 90
+screen -S MySessionName9 -p 0 -X stuff " cd /workspace/George-private/testbed;  python3 ./UnifiedModel_PCPO_PPO_ilp.py --start_sample 90
 "&
-screen -S MySessionName10 -p 0 -X stuff " cd /workspace/George-private/testbed;  python3 ./UnifiedModel_PCPO_PPO_ray_ilp.py --start_sample 100
+screen -S MySessionName10 -p 0 -X stuff " cd /workspace/George-private/testbed;  python3 ./UnifiedModel_PCPO_PPO_ilp.py --start_sample 100
 "
